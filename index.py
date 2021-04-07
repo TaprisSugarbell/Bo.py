2ed5249e20f26274c3173d0af9f0f33e786b21a6
Sugarbell <67601252+TaprisSugarbell@users.noreply.github.com>
2021-04-06 18:16:15 -0600

    General Improvements --/ v0.4

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from telegram.ext import Updater, CommandHandler,  CallbackQueryHandler,Conversa
 
 def start(update, context):
     update.message.reply_text(
-        text='Hola bienvenido a Bopy\n/qr - Genera QR a partir de un texto\n/url - Acorta un link\n/psw - Genera contraseña',
+        text='Hola bienvenido a Bopy\n/qr - Genera QR a partir de un texto\n/url - Acorta un link\n/pwd - Genera contraseña',
         reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton(text='Generar QR', callback_data='qr'), 
             InlineKeyboardButton(text='Acortar URL', callback_data='url')],
