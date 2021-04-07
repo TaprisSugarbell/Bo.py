6cbdbe44d318c9e4ac4f2e11328b6b8cdebd88f0
Sugarbell <67601252+TaprisSugarbell@users.noreply.github.com>
2021-04-06 20:20:14 -0600

    + New Modle --/ pytb

---
+++ modulos/pytb.py
@@ -0,0 +1,41 @@
+import os
+import pytube
+from telegram import ChatAction
+from telegram.ext import ConversationHandler
+
+# Variables
+INPUTpy = 0
+
+def pytbcommand(update, context):
+    update.message.reply_text('Enviame link para descargar video')
+    return INPUTpy
+
+def pytb_callback_handler(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+        text='Enviame link para descargar video'
+    )
+    return INPUTpy
+
+def generate_pytb(text):
+    url = text
+    filename = pytube.YouTube(url).streams.first().download()
+    return filename
+
+def send_pytb(filename, chat):
+    chat.send_action(
+        action=ChatAction.UPLOAD_VIDEO,
+        timeout=None
+        )
+    chat.send_video(
+        video=open(filename, 'rb')
+    )
+    os.unlink(filename)
+
+def input_pytb(update, context):
+    text = update.message.text
+    filename = generate_pytb(text)
+    chat = update.message.chat
+    send_pytb(filename, chat)
+    return ConversationHandler.END
