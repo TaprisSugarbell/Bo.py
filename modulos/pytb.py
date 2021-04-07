7fc5e84e210cd86bc35b951cb2ceee284682eed9
Sugarbell <67601252+TaprisSugarbell@users.noreply.github.com>
2021-04-07 11:34:17 -0600

    pytb.py --/ Improvements --/ v0.01

--- modulos/pytb.py
+++ modulos/pytb.py
@@ -1,5 +1,8 @@
 import os
 import pytube
+import threading
+from queue import Queue
+from datetime import datetime
 from telegram import ChatAction
 from telegram.ext import ConversationHandler
 
@@ -19,8 +22,62 @@ def pytb_callback_handler(update, context):
     return INPUTpy
 
 def generate_pytb(text):
-    url = text
+    with open('cola.txt', 'a') as csf:
+        csf.write(text + '\n')
     filename = pytube.YouTube(url).streams.first().download()
+
+
+    q = Queue()
+    num_threads = 2
+    now = datetime.now()
+
+    with open('cola.txt', 'r') as pyk:
+        pyk = pyk.readlines()
+
+    urls = [
+        pyk
+    ]
+
+    def message(s):
+        print('{}: {}'.format(threading.current_thread().name, s))
+
+    def download(q):
+        while True:
+            message('Buscando en la próxima fila')
+            url = q.get()
+            data = pytube.YouTube(url).title
+            message('Descargando {}'.format(data))
+            filename = pytube.YouTube(url).streams.first().download()
+            # Save the downloaded file to the current directory
+            message('Escribiendo como: {}'.format(data))
+            with open('logs.txt', 'a') as outfile:
+                outfile.write('Se descargo: {} {}\n'.format(data, now))
+            q.task_done()
+
+
+    # Set up some threads
+    for i in range(num_threads):
+        worker = threading.Thread(
+            target=download,
+            args=(q,),
+            name='\nworker-{}'.format(i),
+        )
+        worker.setDaemon(True)
+        worker.start()
+
+    for url in urls:
+        stng = ''.join(url)
+        lstng = stng.split()
+        for msg in lstng:
+            message('queuing {}'.format(
+                msg))
+            q.put(msg)
+
+
+    # Se procesaron todas las descargas.
+    message('*** Cola principal en espera')
+    q.join()
+    message('*** Hecho')
     return filename
 
 def send_pytb(filename, chat):
@@ -32,6 +89,7 @@ def send_pytb(filename, chat):
         video=open(filename, 'rb')
     )
     os.unlink(filename)
+    os.remove('cola.txt')
 
 def input_pytb(update, context):
     text = update.message.text
