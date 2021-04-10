b701b1d6a4d83a6a99f54f030bce017ba7f73ec8
Sugarbell <67601252+TaprisSugarbell@users.noreply.github.com>
2021-04-09 23:08:42 -0600

    Update

--- sample_config.py
+++ sample_config.py
@@ -6,6 +6,7 @@ class Config(object):
     load_dotenv()
     # Obtener un token de bot de botfather
     TOKEN = os.getenv('TOKEN')
-
+    # Obtener un token de deezer
+    ARL = os.getenv('arl')
 
 
