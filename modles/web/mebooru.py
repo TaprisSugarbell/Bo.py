0e3602e28bb8c83e57105e9adebf1799a9a362f6
Sugarbell <anonesayu@gmail.com>
2021-05-05 01:23:17 -0600

    Modle Update --/ 7w7 Version

---
+++ modles/web/mebooru.py
@@ -0,0 +1,50 @@
+import os
+from modles.booru import *
+from telegram.ext import ConversationHandler
+try:
+    from sample_config import Config
+except:
+    from config import Config
+
+# Variables
+Input = 0
+IDuser = int(Config.chatuser)
+
+
+def konachan_callback(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+        text="Ingresa URL"
+    )
+    return Input
+
+
+def send_pic(post, files, chat):
+    caption = caption_kona(post)
+    inline = inline_kona(post)
+    chat_kona(files, caption, inline, chat)
+
+
+def input_konachan(update, context):
+    chat = update.message.chat
+    post = kona_show(context.args[0])
+
+    file = download_kona(post["id"])
+    # Reduciendo Tamaño
+    filejpg = filejpeg(file)
+    # Datos/Envío
+    files = file, filejpg
+    send_pic(post, files, chat)
+    os.unlink(file)
+    os.unlink(filejpg)
+    return ConversationHandler.END
+
+
+
+
+
+
+
+
+
